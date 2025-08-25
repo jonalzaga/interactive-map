84ee8229aace851c109e79dcf979507ea374ae87
JonAlzaga <jonalzaga@hotmail.es>
2025-08-25 17:13:37 +0200

    countries added

--- interactive_map_pandas.py
+++ interactive_map_pandas.py
@@ -1,15 +1,25 @@
 import folium
 import pandas as pd
+import json
 
 # Create the map centered on Ernio
 m = folium.Map(location=[43.1733, -2.1369], zoom_start=10)
+
+
+# Cargar como dict
+
+with open("world.json", encoding="utf-8-sig") as f:
+    data = json.load(f) 
+
+folium.GeoJson(
+    data,
+).add_to(m)
 
 # Read mountain data from a CSV/TXT file
 df = pd.read_csv("mountains_data.txt")
 
 # Iterate over each row in the DataFrame
 for _, row in df.iterrows():
-    print(_)
     name = row["name"]           # Mountain name
     coords = [row["lat"], row["lon"]]  # Latitude and longitude
     climbed = bool(row["climbed"])     # Boolean (True if climbed, False otherwise)
@@ -30,27 +40,30 @@ for _, row in df.iterrows():
     </div>
     """
 
+
     folium.Marker(
-        location=coords,
-        icon=folium.Icon(color=color, icon="flag"),
-        popup=folium.Popup(popup_html, max_width=250)
+       location=coords,
+       icon=folium.Icon(color=color, icon="flag"),
+       popup=folium.Popup(popup_html, max_width=250)
     ).add_to(m)
 
-    # --- Visible label below the marker ---
-    folium.map.Marker(
+    #--- Visible label below the marker ---
+    folium.Marker(
         location=coords,
         icon=folium.DivIcon(
             html=f"""
-                <div style="text-align:center;
-                            transform: translate(-50%, 25px);
-                            font-size: 12px;
-                            font-weight: bold;
-                            color: black;">
-                    {name}
+                <div style="pointer-events:none; text-align:center;
+                transform: translate(-50%, 25px);
+                font-size: 12px;
+                font-weight: bold;
+                color: black;">
+                {name}
                 </div>
-            """
-        )
+                """
+            )
     ).add_to(m)
+
+
 
 # Save the map into an interactive HTML file
 m.save("mountains_pandas.html")
